1596a6c5d3319287d81ff584d1f17a7090f342bb
ARMAN ALI KHAN <72055054+Arman-ali-khan-786@users.noreply.github.com>
2022-10-02 01:15:34 +0530

    code for search bar

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// code for fetching words 
 setTimeout(() => {
     let tableBody = document.getElementById("tablebody");
     const xhr = new XMLHttpRequest();
@@ -28,3 +29,55 @@ setTimeout(() => {
     xhr.send();
 
 }, 100);
+
+// code for search bar
+
+let searchbtn = document.getElementById("searchbtn");
+        searchbtn.addEventListener('click', () => {
+
+            setTimeout(() => {
+                let tableHead = document.getElementById("tablehead");
+                let inputWord = document.getElementById("word");
+                // console.log(inputWord.value);
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', 'wordsapi_sample.json', true);
+                xhr.onload = function () {
+                    if (this.status == 200) {
+                        let obj = JSON.parse(this.responseText);
+                        // console.log(obj)
+                        let str = "";
+                        for (mkey in obj) {
+                            if (obj[mkey].definitions != undefined) {
+                                obj[mkey].definitions.forEach((element, index) => {
+                                    for (const key in element) {
+                                        if (key === 'definition' && mkey.toLowerCase() == inputWord.value.toLowerCase()) {
+                                            str += `<tr scope="row">
+                                                        <td>${mkey}</td>
+                                                        <td>${element[key]}</td>
+                                                    </tr>`
+                                            let message = document.getElementById("message");
+                                            message.innerHTML = `
+                                                                    <div class="alert alert-info alert-dismissible fade show" role="alert">
+                                                                        <strong>Message !</strong> Search results 
+                                                                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                                                    </div>
+                                                                    `;
+                                            setTimeout(() => {
+                                                message.innerHTML = '';
+                                            }, 2000);
+                                        } else {
+
+                                        }
+                                    }
+                                })
+                            };
+
+                        }
+                        tableHead.innerHTML = str;
+                    }
+                }
+                xhr.send();
+
+            }, 100);
+
+        })
